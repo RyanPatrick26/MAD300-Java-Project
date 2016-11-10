47282e63e7f9cf7baa1c1a7b66f8d97a698c0133
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-11-10 16:58:28 -0500

    Added a BorderPane, Title Text, and basic comments for future steps.

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,7 +1,9 @@
 package client;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,13 +15,22 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// Create the form and GUI
+		// Create the initial BorderPane for the main scene
+		BorderPane mainPane = new BorderPane();
+		// Create the title for the primary stage
+		Text formTitle = new Text("Bearham Game Manager");
 		
+		
+		
+		
+		
+		// Create the scene for the previously played games
+		//	must store the game, the genre(s), and the date played
 		
 		
 		Pane pane = new Pane();
 		
-		Scene scene = new Scene(pane, 800, 600);
+		Scene scene = new Scene(mainPane, 800, 600);
 		
 		primaryStage.setTitle("[MAD300 Java Project]");
 		primaryStage.setScene(scene);
