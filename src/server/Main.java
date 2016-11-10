7c97f549406dc434de0e345962f71c9e87aabb8c
Brandon Brown <brandon.brown22@stclairconnect.ca>
2016-11-10 17:37:10 -0500

    Added ability to connect to database
    The user can now use the database.properties file to connect to a database.
    Currently, the application prints all tables in the database to the screen.

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,13 +1,55 @@
 package server;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
 public class Main {
 
-	public static void main(String[] args) {
-		DatabaseConfig dbconf = new DatabaseConfig();
-		System.out.println(dbconf.getDatabaseHost());
-		System.out.println(dbconf.getDatabaseName());
-		System.out.println(dbconf.getDatabasePass());
-		System.out.println(dbconf.getDatabaseUser());
+	static DatabaseConfig dbconf = new DatabaseConfig();
+	
+	public static void main(String[] args) {	
+		initDB();
 	}
-
+	
+	static Connection connection;
+	
+	protected static void initDB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://" + 
+				dbconf.getDatabaseHost() + "/" +
+				dbconf.getDatabaseName() + "?useSSL=false",
+				dbconf.getDatabaseUser(),
+				dbconf.getDatabasePass()
+			);
+			
+			try {
+				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement("SHOW TABLES");
+				preparedStatement.execute();
+				ResultSet result = preparedStatement.getResultSet();
+				
+				while (result.next()) {
+					System.out.println(result.getString("Tables_in_" + dbconf.getDatabaseName()));
+				}
+			} catch (SQLException e) {
+				System.out.println("SQL Exception: " + e.getMessage());
+				System.out.println("SQL State: " + e.getSQLState());
+				System.out.println("SQL Error Code: " + e.getErrorCode());
+			}
+			
+			connection.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Looks like SQL had an oopsie!");
+		}
+	}
 }
