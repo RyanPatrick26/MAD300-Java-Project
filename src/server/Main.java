4a5f66c40ed8877ed402520cf461d0de5de63cc4
Brandon Brown <brandon.brown22@stclairconnect.ca>
2016-11-10 18:18:32 -0500

    Can check/add missing tables
    The application can check if a table exists, and if neccessary, create the table
    TODO: Should allow createTable() to take an array of parameters and dynamically create the table

--- src/server/Main.java
+++ src/server/Main.java
@@ -17,7 +17,7 @@ public class Main {
 	
 	static Connection connection;
 	
-	protected static void initDB() {
+	public static boolean tableExists(String tableName) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = (Connection) DriverManager.getConnection("jdbc:mysql://" + 
@@ -28,13 +28,16 @@ public class Main {
 			);
 			
 			try {
-				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement("SHOW TABLES");
+				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement("SHOW TABLES LIKE '" + tableName + "'");
 				preparedStatement.execute();
 				ResultSet result = preparedStatement.getResultSet();
 				
-				while (result.next()) {
-					System.out.println(result.getString("Tables_in_" + dbconf.getDatabaseName()));
+				if (result.next()) {
+					return true;
+				} else {
+					return false;
 				}
+				
 			} catch (SQLException e) {
 				System.out.println("SQL Exception: " + e.getMessage());
 				System.out.println("SQL State: " + e.getSQLState());
@@ -50,6 +53,65 @@ public class Main {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
 			System.out.println("Looks like SQL had an oopsie!");
+		}
+		return false;
+	}
+	
+	public static void createTable(String tableName) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://" + 
+				dbconf.getDatabaseHost() + "/" +
+				dbconf.getDatabaseName() + "?useSSL=false",
+				dbconf.getDatabaseUser(),
+				dbconf.getDatabasePass()
+			);
+			
+			try {
+				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(
+					"CREATE TABLE " + tableName + "(" +
+							"ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+							"GameName VARCHAR(32)," +
+							"Rating VARCHAR(16)," +
+							"Description TEXT" +
+							");"
+					);
+				preparedStatement.execute();
+			} catch (SQLException e) {
+				System.out.println("SQL Exception: " + e.getMessage());
+				System.out.println("SQL State: " + e.getSQLState());
+				System.out.println("SQL Error Code: " + e.getErrorCode());
+				System.out.println("[SHUTTING DOWN]");
+				System.exit(0);
+			}
+			
+			connection.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Looks like SQL had an oopsie!");
+		}
+	}
+	
+	protected static void initDB() {
+		
+		if (tableExists("GameManagement")) {
+			System.out.println("Table Exists!");
+		} else {
+			System.out.println("Table Doesn't Exist!");
+			System.out.println("Creating Table...");
+			createTable("GameManagement");
+			if (tableExists("GameManagement")) {
+				System.out.println("Table Exists!");
+			} else {
+				System.out.println("Table Still Doesn't Exist!");
+				System.out.println("[SHUTTING DOWN]");
+				System.exit(0);
+			}
 		}
 	}
 }
