60366c6643599b89e5200bcfa0c626e8c7ea168e
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-11-10 18:25:06 -0500

    Added a method to the BackButton class to set a mouse click event to it.

--- src/client/BackButton.java
+++ src/client/BackButton.java
@@ -1,15 +1,45 @@
 package client;
 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class BackButton extends BorderPane {
-	private String backButtonName;
+	private String backButtonString;
 	private Image backButtonImage = null;
 	
-	public BackButton(String buttonName, Image buttonImage) {
-		Text backButtontext = new Text("Back");
+	public BackButton(String backButtonString, Image backButtonImage) {
+		this.backButtonString = backButtonString;
+		this.backButtonImage = backButtonImage;
+	
+		Text backButtonText = new Text(backButtonString);
+		
+		// Check to see if an image has been passed
+		if(this.backButtonImage != null){
+			this.setCenter(new ImageView(this.backButtonImage));
+		}
+		else {
+			this.setCenter(backButtonText);
+		}
+		
+	}
+	
+	/**
+	 * Sets an OnClick event to the BackButton that sets the scene passed Stage parameter
+	 * to the passed Scene parameter and shows the Stage.
+	 * 
+	 * @author Nicholas Allaire
+	 * @param Stage stage
+	 * @param Scene scene
+	 */
+	public void setBackButtonEvent(Stage stage, Scene scene) {
+		this.setOnMouseClicked(e -> {
+			stage.setScene(scene);
+			stage.show();
+		});
 	}
 	
 }
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,10 +1,11 @@
 package client;
 import java.io.File;
 import java.util.ArrayList;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -13,14 +14,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	ArrayList<String> categories;
+	static BackButton customBackButton;
 	
 	public static void main(String[] args) {
 		Application.launch(args);
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// must store the game name, the genre(s), date last played, publisher, year the game was released
+		
 		/* MAIN SCREEN */
 		
 		// Create the title for the primary stage
@@ -28,13 +31,7 @@ public class Main extends Application {
 		
 		// Create the form to add a new game to the game manager
 		Text addGameText = new Text("Add New Game");
-		
-		
-		
-		
-		
-		
-		
+
 		// Create the button to go the Previous games list
 		Button previousGamesButton = new Button("Previously Played Games");
 		
@@ -47,38 +44,39 @@ public class Main extends Application {
 		
 		// Create the BorderPane for the main scene
 		BorderPane mainPane = new BorderPane();
+		mainPane.setPadding(new Insets(10,10,10,10));
 		mainPane.setTop(formTitleText);
 		mainPane.setCenter(addGameText);
 		mainPane.setBottom(bottomBox);
 		
-		/* PREVIOUS GAMES SCREEN */
+		// Create the scene for the main screen
+		Scene mainScene = new Scene(mainPane, 800, 600);
 		
-		// Create the BorderPane for the previous games scene
-		// must store the game name, the genre(s), date last played, publisher, year the game was released
-		BorderPane previousGamesPane = new BorderPane();
+		/* PREVIOUS GAMES SCREEN */
 		
 		// Create the back button
 		File backImage = new File("images/back-arrow-icon.png");
 		if(backImage.isFile()) {
-			
+			customBackButton = new BackButton("Back", new Image("images/back-arrow-icon.png"));
+			// Assign the event handler for the Back Button
+			customBackButton.setBackButtonEvent(primaryStage, mainScene);
 		} else {
-			Button backButton = new Button("");
+			Button backButton = new Button("Back");
 		}
 		
-		
-		/* SCENES & STAGE */
+		// Create the BorderPane for the previous games scene
+		BorderPane previousGamesPane = new BorderPane();
+		previousGamesPane.setTop(customBackButton);
 		
 		// Create the main and previousGames scenes
 		Scene previousGamesScene = new Scene(previousGamesPane, 800, 600);
-		Scene mainScene = new Scene(mainPane, 800, 600);
+		
 		
 		// Create event handler for the Previous Games Button
 		previousGamesButton.setOnAction(e -> {
 			primaryStage.setScene(previousGamesScene);
 			primaryStage.show();
 		});
-		// Create event handler for the Back Button
-		
 		
 		primaryStage.setTitle("[MAD300 Java Project]");
 		primaryStage.setScene(mainScene);
