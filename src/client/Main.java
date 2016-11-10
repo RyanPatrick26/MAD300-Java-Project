3325960d927105bc74e209e476d553c9e17b9bef
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-11-10 17:10:49 -0500

    Created a button to switch to viewing previous games.

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,6 +1,7 @@
 package client;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -15,25 +16,40 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// Create the initial BorderPane for the main scene
-		BorderPane mainPane = new BorderPane();
 		// Create the title for the primary stage
-		Text formTitle = new Text("Bearham Game Manager");
+		Text formTitleText = new Text("Bearham Game Manager");
+		
+		// Create the form to add a new game to the game manager
+		Text addGameText = new Text("Add New Game");
 		
 		
 		
 		
+		
+		
+		
+		// Create the button to go the Previous games list
+		Button previousGamesButton = new Button("Previously Played Games");
+		
+		// Create the copyright information Text
+		Text copyrightText = new Text("Copyright Team Bearham - 2016");
+		
+		// Create an HBox to store the bottom of the BorderPane 
 		
 		// Create the scene for the previously played games
-		//	must store the game, the genre(s), and the date played
+		//must store the game name, the genre(s), date last played, publisher, year the game was released
 		
 		
-		Pane pane = new Pane();
+		// Create the BorderPane for the main scene
+		BorderPane mainPane = new BorderPane();
+		mainPane.setTop(formTitleText);
+		mainPane.setCenter(addGameText);
+		mainPane.setBottom(copyrightText);
 		
-		Scene scene = new Scene(mainPane, 800, 600);
+		Scene mainScene = new Scene(mainPane, 800, 600);
 		
 		primaryStage.setTitle("[MAD300 Java Project]");
-		primaryStage.setScene(scene);
+		primaryStage.setScene(mainScene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
 		
