033a8cdfbd40e6c0495459cc7460da98385dbaed
Ryan Patrick <ryan.patrick26@stclairconnect.ca>
2016-11-16 18:23:41 -0500

    Completed functionality for selecting games by genre

--- src/client/Main.java
+++ src/client/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
@@ -19,6 +20,7 @@ public class Main extends Application {
 	ArrayList<String> categoryList;
 	ArrayList<Game> gameList;
 	ListView<Game> gameListView;	
+	ObservableList<Game> selectedGameList;
 	
 	public static void main(String[] args) {
 		Application.launch(args);
@@ -36,33 +38,77 @@ public class Main extends Application {
 		gameList.add(new Game("Risk", new ArrayList<String>(Arrays.asList(categoryList.get(0)))));
 		gameList.add(new Game("Magic the Gathering", new ArrayList<String>(Arrays.asList(categoryList.get(1), categoryList.get(2)))));
 		gameList.add(new Game("Mansions of Madness", new ArrayList<String>(Arrays.asList(categoryList.get(0), categoryList.get(2)))));
-		gameList.add(new Game("Exceed", new ArrayList<String>(Arrays.asList(categoryList.get(0), categoryList.get(1)))));
-		gameList.add(new Game("Total War: Warhammer", new ArrayList<String>(Arrays.asList(categoryList.get(2)))));
+		gameList.add(new Game("Exceed", new ArrayList<String>(Arrays.asList(categoryList.get(0), categoryList.get(2)))));
+		gameList.add(new Game("Total War: Warhammer", new ArrayList<String>(Arrays.asList(categoryList.get(1)))));
 		gameList.add(new Game("Splendor", new ArrayList<String>(Arrays.asList(categoryList.get(0)))));
 		
 		CheckBox boardGame = new CheckBox("Board Games");
 		CheckBox videoGame = new CheckBox("Video Games");
 		CheckBox cardGame = new CheckBox("Card Games");
-		
-		ArrayList<Game> selectedGameList = new ArrayList<Game>();
+
+		selectedGameList = FXCollections.observableList(new ArrayList<Game>());
 		gameListView = new ListView<Game>();
 		
 		boardGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
 			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+				ArrayList<Game> boardGameList = new ArrayList<Game>();
 				for(int i = 0; i < gameList.size(); i++){
 					if(newValue){
 						if(gameList.get(i).getCategory().contains("Board Game")){
-							System.out.println(gameList.get(i));
-							selectedGameList.add(gameList.get(i));
+							if(!selectedGameList.contains(gameList.get(i))){
+								selectedGameList.add(gameList.get(i));
+							}
 						}
 					}
 					else{
 						if(gameList.get(i).getCategory().contains("Board Game")){
-							System.out.println(gameList.get(i));
 							selectedGameList.remove(gameList.get(i));
 						}
 					}
 				}
+				gameListView.setItems(selectedGameList);
+
+				//selectedGameList.removeAll(boardGameList);
+			}
+		});
+		videoGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
+			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+				ArrayList<Game> videoGameList = new ArrayList<Game>();
+				for(int i = 0; i < gameList.size(); i++){
+					if(newValue){
+						if(gameList.get(i).getCategory().contains("Video Game")){
+							if(!selectedGameList.contains(gameList.get(i))){
+								selectedGameList.add(gameList.get(i));
+							}
+						}
+					}
+					else{
+						if(gameList.get(i).getCategory().contains("Video Game")){
+							selectedGameList.remove(gameList.get(i));
+						}
+					}
+				}
+				gameListView.setItems(selectedGameList);
+			}
+		});
+		cardGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
+			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+				ArrayList<Game> cardGameList = new ArrayList<Game>();
+				for(int i = 0; i < gameList.size(); i++){
+					if(newValue){
+						if(gameList.get(i).getCategory().contains("Card Game")){
+							if(!selectedGameList.contains(gameList.get(i))){
+								selectedGameList.add(gameList.get(i));
+							}
+						}
+					}
+					else{
+						if(gameList.get(i).getCategory().contains("Card Game")){
+							selectedGameList.remove(gameList.get(i));
+						}
+					}
+				}
+				gameListView.setItems(selectedGameList);
 			}
 		});
 		
