58a392d056ee0ca7ddb4a782ededc0913f5a1b3a
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-11-23 18:11:36 -0500

    #3 & #6: Tidied up code. - Aligned Labels - Fixed button positions - Fixed GridPane positioning Ready to merge into staging.

--- src/client/GameForm.java
+++ src/client/GameForm.java
@@ -1,112 +1,160 @@
 package client;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class GameForm extends GridPane {
-	
+
 	private Label gameTitleLabel;
 	private TextField gameTitleField;
-	
 	private Label genreLabel;
-	
 	private Label yearLabel;
 	private TextField yearField;
-	
 	private Label publisherLabel;
 	private TextField publisherField;
-	
-	
 	private ObservableList<String> genres;
 	private ListView<String> genreList;
-	
-	
+
+	private Label hoursPlayedLabel;
+	private TextField hoursPlayedField;
+
+	private Label gameDescriptionLabel;
+	private TextArea gameDescriptionArea;
+
 	public GameForm() {
 		// Create default styling for the GameForm
-		this.setPadding(new Insets(15,10,15,10));
+		this.setPadding(new Insets(15, 10, 15, 10));
 		this.setVgap(15);
 		this.setHgap(15);
-		
+
 		// Set the game title label and add it to the grid
 		this.gameTitleLabel = new Label("Game Title:");
-		this.add(gameTitleLabel,0,2);
-		
+		this.add(gameTitleLabel, 0, 2);
+
 		// Set the textfield for the game title and add it to the grid
 		this.gameTitleField = new TextField();
-		this.add(gameTitleField,3,2);
-		
+		this.add(gameTitleField, 3, 2);
+
 		// Set the genre label and add it to the grid
-		this.genreLabel = new Label("Genre(s) - Shift click to select more:");
+		this.genreLabel = new Label("Genre(s) - Shift click to select more than 1:");
 		this.add(genreLabel, 0, 3);
-		
+
 		// Create an observable array list for the game genres
-		this.genres = FXCollections.observableArrayList("Board Game","Card Game","Video Game");
+		this.genres = FXCollections.observableArrayList("Board Game", "Card Game", "Video Game");
 		// Put the observable array list in a listView
 		this.genreList = new ListView<>(genres);
 		this.genreList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		this.genreList.setMaxHeight(75);
 		// Add the listview to the gridpane
-		this.add(genreList,3,3);
-		
+		this.add(genreList, 3, 3);
+
 		/**
-		 * When a list item is selected, the currently selected genre or genres will
-		 * be displayed to the console. This is for testing purposes.
+		 * When a list item is selected, the currently selected genre or genres
+		 * will be displayed to the console. This is for testing purposes.
+		 * 
 		 * @author Nicholas Allaire
 		 * @param None
 		 */
-		this.genreList.setOnMouseClicked(e->{
+		this.genreList.setOnMouseClicked(e -> {
 			ObservableList<String> selectedGenres = genreList.getSelectionModel().getSelectedItems();
-			for(String genreString : selectedGenres) {
+			for (String genreString : selectedGenres) {
 				System.out.println(genreString);
 			}
 		});
-		
+
 		// Set the game release year label and add it to the grid
 		yearLabel = new Label("Year Released:");
-		this.add(yearLabel,0,4);
-		
-		// Set the textfield for the game release year and add it to the grid 
+		this.add(yearLabel, 0, 4);
+
+		// Set the textfield for the game release year and add it to the grid
 		this.yearField = new TextField();
-		this.add(yearField,3,4);
-		
+		this.add(yearField, 3, 4);
+
 		// Set the publisher label and add it to the grid
 		this.publisherLabel = new Label("Game Publisher:");
-		this.add(publisherLabel,0,5);
-		
+		this.add(publisherLabel, 0, 5);
+
 		// Set the textfield for the publisher and add it to the grid
 		this.publisherField = new TextField();
-		this.add(publisherField,3,5);
-		
+		this.add(publisherField, 3, 5);
+
+		// Set the label for the hours played and add it to the grid
+		hoursPlayedLabel = new Label("Hours Played:");
+		this.add(hoursPlayedLabel, 0, 6);
+
+		// Set the textfield for the hours played and add it to the grid
+		hoursPlayedField = new TextField("");
+		this.add(hoursPlayedField, 3, 6);
+
+		// Set the label for the game description
+		gameDescriptionLabel = new Label("Game Description:");
+		GridPane.setHalignment(gameDescriptionLabel, HPos.LEFT);
+		GridPane.setValignment(gameDescriptionLabel, VPos.TOP);
+		this.add(gameDescriptionLabel, 0, 7);
+
+		// Set the textarea for the game description and add it to the grid
+		gameDescriptionArea = new TextArea();
+		gameDescriptionArea.setMaxSize(320, 100);
+		this.add(gameDescriptionArea, 3, 7);
+
 		// Create the submit and clear buttons
 		Button submitButton = new Button("Submit Game");
-		submitButton.setMinWidth(50);
+		submitButton.setMinWidth(100);
 		Button clearButton = new Button("Clear Form");
-		
+		clearButton.setMinWidth(100);
+
 		// Give the buttons functionality
-		submitButton.setOnAction(e->{
+		/**
+		 * Clears the form on button press. TODO: Add database functionality.
+		 * 
+		 * @author Nicholas Allaire
+		 * @param None
+		 */
+		submitButton.setOnAction(e -> {
 			gameTitleField.clear();
 			genreList.getSelectionModel().clearSelection();
 			yearField.clear();
 			publisherField.clear();
-			
+			hoursPlayedField.clear();
+			gameDescriptionArea.clear();
+
 		});
-		clearButton.setOnAction(e->{
+		/**
+		 * Clears the form on button press.
+		 * 
+		 * @author Nicholas Allaire
+		 * @param None
+		 */
+		clearButton.setOnAction(e -> {
 			gameTitleField.clear();
 			genreList.getSelectionModel().clearSelection();
 			yearField.clear();
 			publisherField.clear();
+			hoursPlayedField.clear();
+			gameDescriptionArea.clear();
 		});
-		
+
+		// Create HBox
+		HBox buttonBox = new HBox();
+		buttonBox.getChildren().addAll(submitButton, clearButton);
+		buttonBox.setPadding(new Insets(5, 5, 5, 5));
+		buttonBox.setSpacing(15);
+		GridPane.setColumnSpan(buttonBox, 2);
+
 		// Add the buttons to the grid
-		this.add(submitButton, 1, 7);
-		this.add(clearButton, 2, 7);
-		
+		this.add(buttonBox, 1, 9);
+
 	}
 
 }
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,13 +1,10 @@
 package client;
 
-import java.awt.Desktop;
 import java.io.File;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -22,12 +19,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -36,13 +29,13 @@ public class Main extends Application {
 	private static Button backButton;
 	ArrayList<String> categoryList;
 	ArrayList<Game> gameList;
-	ListView<Game> gameListView;	
+	ListView<Game> gameListView;
 	ArrayList<Game> selectedGameList = new ArrayList<Game>();
-	
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// must store the game name, the genre(s), date last played, publisher,
@@ -52,15 +45,15 @@ public class Main extends Application {
 
 		// Create the title for the primary stage
 		Text formTitleText = new Text("Bearham Game Manager");
-		
+
 		// Create the text to prompt the user to add a new game
 		Text addGameText = new Text("Add New Game");
-		
+
 		// Add the form title and addGameText to a VBox
 		VBox topBox = new VBox();
 		topBox.getChildren().addAll(formTitleText, addGameText);
 		topBox.setAlignment(Pos.CENTER);
-		topBox.setPadding(new Insets(10,10,10,10));
+		topBox.setPadding(new Insets(10, 10, 10, 10));
 		topBox.setSpacing(10);
 
 		// Create the form to add a new game to the game manager
@@ -95,7 +88,7 @@ public class Main extends Application {
 		BorderPane.setAlignment(formTitleText, Pos.CENTER);
 
 		// Create the scene for the main screen
-		Scene mainScene = new Scene(mainPane, 800, 600);
+		Scene mainScene = new Scene(mainPane, 800, 700);
 
 		/* PREVIOUS GAMES SCREEN */
 
@@ -128,11 +121,12 @@ public class Main extends Application {
 		}
 
 		// Create the main and previousGames scenes
-		Scene previousGamesScene = new Scene(previousGamesPane, 800, 600);
+		Scene previousGamesScene = new Scene(previousGamesPane, 800, 700);
 
 		// Create event handler for the Previous Games Button
 		/**
 		 * Switches to the previously played games screen
+		 * 
 		 * @author Nicholas Allaire
 		 * @param None
 		 */
@@ -140,103 +134,99 @@ public class Main extends Application {
 			primaryStage.setScene(previousGamesScene);
 			primaryStage.show();
 		});
-		
+
 		categoryList = new ArrayList<String>();
 		gameList = new ArrayList<Game>();
-		
+
 		categoryList.add("Board Game");
 		categoryList.add("Video Game");
 		categoryList.add("Card Game");
-		
+
 		gameList.add(new Game("Risk", new ArrayList<String>(Arrays.asList(categoryList.get(0)))));
 		gameList.add(new Game("Archeage", new ArrayList<String>(Arrays.asList(categoryList.get(1)))));
 		gameList.add(new Game("Magic the Gathering", new ArrayList<String>(Arrays.asList(categoryList.get(2)))));
-		
-		
-		
+
 		CheckBox boardGame = new CheckBox("Board Games");
 		CheckBox videoGame = new CheckBox("Video Games");
 		CheckBox cardGame = new CheckBox("Card Games");
-		
-		
+
 		gameListView = new ListView<Game>();
-		
-		boardGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
-			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+
+		boardGame.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue) {
 				buildListView(newValue, "Board Game");
 			}
-				
+
 		});
-		
-		videoGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
-			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+
+		videoGame.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue) {
 				buildListView(newValue, "Video Game");
 			}
 		});
-		
-		cardGame.selectedProperty().addListener(new ChangeListener<Boolean>(){
-			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue){
+
+		cardGame.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue a, Boolean oldValue, Boolean newValue) {
 				buildListView(newValue, "Card Game");
 			}
 		});
-		
+
 		VBox categoryBox = new VBox();
 		categoryBox.getChildren().addAll(boardGame, videoGame, cardGame);
-		
+
 		HBox pane = new HBox();
 		pane.getChildren().addAll(categoryBox, gameListView);
-		
+
 		// Add the categoryBox and gameListView to the previous games screen
 		previousGamesPane.setCenter(pane);
-		
+
 		primaryStage.setTitle("[MAD300 Java Project]");
 		primaryStage.setScene(mainScene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
 
 	}
-	
+
 	/**
-	   * Custom Function to sort the selectedGameList alphabetically.
-	   * No inputs needed, just needs to be set as the comparator to use.
-	   * Added by: Megan Caza 
+	 * Custom Function to sort the selectedGameList alphabetically. No inputs
+	 * needed, just needs to be set as the comparator to use. Added by: Megan
+	 * Caza
 	 **/
 	public class CustomComparator implements Comparator<Game> {
-	    @Override
-	    public int compare(Game o1, Game o2) {
-	        return o1.getName().compareTo(o2.getName());
-	    }
+		@Override
+		public int compare(Game o1, Game o2) {
+			return o1.getName().compareTo(o2.getName());
+		}
 	}
-	
+
 	/**
-	   * Custom Function used to find the selected game category, search through the games created, add games to the selectedGameList,
-	   * sort the selectedGameList alphabetically, and insert selectedGameList into the listview to display to the screen.
-	   * INPUTS: boolean newValue, String gameType
-	   * OUTPUTS: void
-	   * Created by: Megan Caza 
+	 * Custom Function used to find the selected game category, search through
+	 * the games created, add games to the selectedGameList, sort the
+	 * selectedGameList alphabetically, and insert selectedGameList into the
+	 * listview to display to the screen. INPUTS: boolean newValue, String
+	 * gameType OUTPUTS: void Created by: Megan Caza
 	 **/
-	public void buildListView(boolean newValue, String gameType){
-		for(int i = 0; i < gameList.size(); i++){
-			if(newValue){
-				if(gameList.get(i).getCategory().contains(gameType)){
+	public void buildListView(boolean newValue, String gameType) {
+		for (int i = 0; i < gameList.size(); i++) {
+			if (newValue) {
+				if (gameList.get(i).getCategory().contains(gameType)) {
 					selectedGameList.add(gameList.get(i));
 					Collections.sort(selectedGameList, new CustomComparator());
 					gameListView.getItems().clear();
-					ObservableList<Game> items =FXCollections.observableArrayList (selectedGameList);
+					ObservableList<Game> items = FXCollections.observableArrayList(selectedGameList);
 					gameListView.setItems(items);
 				}
-			}
-			else{
-				if(gameList.get(i).getCategory().contains(gameType)){
+			} else {
+				if (gameList.get(i).getCategory().contains(gameType)) {
 					selectedGameList.remove(gameList.get(i));
 					Collections.sort(selectedGameList, new CustomComparator());
 					gameListView.getItems().clear();
-					ObservableList<Game> items =FXCollections.observableArrayList (selectedGameList);
+					ObservableList<Game> items = FXCollections.observableArrayList(selectedGameList);
 					gameListView.setItems(items);
 				}
 			}
 		}
-	
+
 	}
 
 }
