a273b71a7e03556c408f85fce63e0cf5fcf7c244
Brandon Brown <brandon.brown22@stclairconnect.ca>
2016-11-24 18:04:00 -0500

    #19: Updated/Added comments

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,17 +6,29 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Master server class, used for all instances of server threads
+ * @author Brandon Brown
+ */
 public class Server extends Thread {
+	
 	DataInputStream input;
 	DataOutputStream output;
 	ServerSocket listener;
 	
+	/**
+	 * Server constructor, creates threads to handle all connections
+	 * @throws IOException
+	 */
 	public Server() throws IOException {
 		try {
+			// TODO: Add the server socket's port to a configuration file.
 			listener = new ServerSocket(2000);
+			
+			// Main server loop
 			while (true) {	
 				Socket socket = listener.accept();
-				System.out.println("[ SERVER ] READY!");
+				System.out.println("[ SERVER ] Accepted Connection");
 				ServerThread serverThread = new ServerThread(socket);
 				Thread thread = new Thread(serverThread);
 				thread.start();
@@ -26,7 +38,15 @@ public class Server extends Thread {
 		}
 	}
 	
+	/**
+	 * Entry point for application, only purpose is to launch the server
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
+		// TODO: Make use of application arguments to change the behavior of server
+		
+		// Start the server as soon as the application is launched.
 		new Server();
 	}
 }
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -6,12 +6,23 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * Server thread for every new connection to the server
+ * This is where all the communication between clients happens
+ * @author Brandon Brown
+ *
+ */
 public class ServerThread implements Runnable {
 
 	DataInputStream input;
 	DataOutputStream output;
 	Socket socket;
 	
+	/**
+	 * ServerThread constructor
+	 * @param socket	is passed from the Master Server to it's
+	 * 					threads for handling of the client's connection
+	 */
 	public ServerThread(Socket socket) {
 		this.socket = socket;
 	}
@@ -24,10 +35,12 @@ public class ServerThread implements Runnable {
 			while (true) {
 				input = new DataInputStream(socket.getInputStream());
 				output = new DataOutputStream(socket.getOutputStream());
-				
 				if (input.available() > 0) {
 					try {
 						String message = input.readUTF();
+						
+						System.out.println(message);
+						
 						PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 						
 						out.println("[" + i + "] " + message);
